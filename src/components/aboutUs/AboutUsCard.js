f87761d72869679911e7087976492f4b9ad73e49
Michael Barreau <davidb153@gmail.com>
2022-03-12 12:30:21 -0600

    added components, rearrange css

---
+++ src/components/aboutUs/AboutUsCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./AboutUs.css"
+
+export const AboutUsCard = () => (
+    <section className="aboutUs">
+        <h3 className="aboutUs">About Us</h3>
+        <div className="abousUs">Learn more ASAP: IFYKYK </div>
+    </section>
+)
+
